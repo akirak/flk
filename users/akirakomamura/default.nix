f749baa4f9dfc723b5419fc69646895611013e13
Akira Komamura <akira.komamura@gmail.com>
2021-08-28 22:48:55 +0900

    Add linkfarm services

--- users/akirakomamura/default.nix
+++ users/akirakomamura/default.nix
@@ -4,12 +4,15 @@
     uid = 1000;
     description = "default";
     isNormalUser = true;
-    extraGroups = [ "wheel" "video" "audio" "disk" "networkmanager" ];
+    extraGroups = [ "wheel" "video" "audio" "disk" "networkmanager" "systemd-journal" ];
     hashedPassword = "$6$4UhguaItTVfvZm0$0UTUCTHVtxM8I7.gfBD/mJaXYmHGkqWSYf6.q6P49VS7FmKWIjdUlFPuuc9Ap57.p5299xZ/T33zb/3o3T317.";
   };
 
   home-manager.users.akirakomamura = { suites, ... }: {
-    imports = suites.development;
+    imports = suites.development ++ [
+      ../profiles/linkfarm
+      ../profiles/linkfarm-annex
+    ];
 
     programs.git = {
       userEmail = "akira.komamura@gmail.com";
