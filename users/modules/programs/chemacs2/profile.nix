195e666870e126b05cdc2d639fc8e314b2734fc3
Akira Komamura <akira.komamura@gmail.com>
2021-08-31 22:33:44 +0900

    chemacs: Accept a flake app name

--- users/modules/programs/chemacs2/profile.nix
+++ users/modules/programs/chemacs2/profile.nix
@@ -1,23 +1,26 @@
-{
-  makeWrapper,
-  runCommandNoCC,
-  symlinkJoin,
-  makeDesktopItem,
-  writeShellScriptBin,
-  runtimeShell,
-  lib,
-  emacs,
-  homeDirectory,
-  # Use 'nix run' instead of package
-  useNixRun ? false,
-  # Name of the profile
-  name,
-  # Emacs package
-  package ? emacs,
-  # The value of user-emacs-directory
-  directory,
-  # The value of custom-file (optional)
+{ makeWrapper
+, runCommandNoCC
+, symlinkJoin
+, makeDesktopItem
+, writeShellScriptBin
+, runtimeShell
+, lib
+, emacs
+, homeDirectory
+, # Use 'nix run' instead of package
+  useNixRun ? false
+, # Name of the profile
+  name
+, # Emacs package
+  package ? emacs
+, # The value of user-emacs-directory
+  directory
+, # The value of custom-file (optional)
   customFile ? null
+, # The name of the flake app starting with '#', if you use nix run
+  appName ? ""
+, # The app that runs emacsclient, if any
+  clientAppName ? null
 }:
 let
   expandTilde = str:
@@ -34,37 +37,53 @@ let
   profileArg =
     "(" +
     lib.concatStrings
-    (lib.mapAttrsToList (name: value:
-      if value == null
-      then ""
-      else '' (${name} . "${value}") ''
-    ) profileAttrs)
+      (lib.mapAttrsToList
+        (name: value:
+          if value == null
+          then ""
+          else '' (${name} . "${value}") ''
+        )
+        profileAttrs)
     + ")";
 
-  normalDerivation = runCommandNoCC "emacs-profile-${name}" {
+  normalDerivation = runCommandNoCC "emacs-profile-${name}"
+    {
+      preferLocalBuild = true;
+      nativeBuildInputs = [
+        makeWrapper
+      ];
+      propagatedBuildInputs =
+        if useNixRun
+        then [ ]
+        else [ package ];
+    } ''
+    mkdir -p $out/bin
+    makeWrapper ${package}/bin/emacs $out/bin/emacs-${name} \
+      --add-flags "--with-profile '${profileArg}'"
+    makeWrapper ${package}/bin/emacsclient $out/bin/emacsclient-${name} \
+      --add-flags "-s ${name}"
+  '';
+
+  indirectCallDerivation = symlinkJoin {
+    name = "emacs-profile-${name}-run";
     preferLocalBuild = true;
-    nativeBuildInputs = [
-      makeWrapper
-    ];
-    propagatedBuildInputs =
-      if useNixRun
-      then []
-      else [ package ];
-  } ''
-      mkdir -p $out/bin
-      makeWrapper ${package}/bin/emacs $out/bin/emacs-${name} \
-        --add-flags "--with-profile '${profileArg}'"
-      makeWrapper ${package}/bin/emacsclient $out/bin/emacsclient-${name} \
-        --add-flags "-s ${name}"
-  '';
+    paths = [
+      (writeShellScriptBin "emacs-${name}" ''
+        exec nix run --no-update-lock-file "${expandTilde directory}${appName}" \
+          -- --with-profile '${profileArg}' "$@"
+      '')
+    ] ++ (lib.optional (clientAppName != null)
+      (writeShellScriptBin "emacsclient-${name}"
+        ''
+          exec nix run --no-update-lock-file "${expandTilde directory}${clientAppName}" \
+            -- --with-profile '${profileArg}' "$@"
+        ''));
+  };
 
-  indirectCall = writeShellScriptBin "emacs-${name}" ''
-    exec nix run --no-update-lock-file "${expandTilde directory}" -- --with-profile '${profileArg}' "$@"
-  '';
-
-  drv = if useNixRun
-        then indirectCall
-        else normalDerivation;
+  drv =
+    if useNixRun
+    then indirectCallDerivation
+    else normalDerivation;
 
 in
 symlinkJoin {
