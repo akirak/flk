892f3a32da98cbe9e68153ac533c55f1dd155e56
Akira Komamura <akira.komamura@gmail.com>
2021-09-02 01:30:46 +0900

    xmonad: Add per-workspace action

--- users/profiles/xmonad/xmonad.hs
+++ users/profiles/xmonad/xmonad.hs
@@ -27,7 +27,10 @@ main = do
     , layoutHook = myLayoutHook
     , manageHook = myManageHook
     , handleEventHook = myHandleEventHook
-    , workspaces = [show i | i <- [1..9]]
+    , workspaces =
+      ["1-emacs", "2-web"] ++
+      [show i | i <- [3..7]] ++
+      ["8-music", "9-github", "0-monday"]
     , logHook = myPolybarLogHook dbus
     }
     `additionalKeys` myKeybindings
@@ -60,9 +63,16 @@ myKeybindings =
   , (key, c) <- [(xK_w, onPrevNeighbour), (xK_e, onNextNeighbour)]
   ]
   ++
-  [ ((modm, key), action i)
-  | (modm, action) <- [(mod4Mask, viewWorkspaceByPrefix . show), (mod4Mask .|. shiftMask, shiftToWorkspaceByPrefix . show)]
-  , (key, i) <- zip [xK_1..xK_9] [1..9]
+  [ ((mod4Mask, key), do viewWorkspaceByPrefix (show i)
+                         withWindowSet $ \s ->
+                           case W.peek s of
+                             Nothing -> runWorkspaceAction
+                             _ -> return ())
+  | (key, i) <- (zip [xK_1..xK_9] [1..9]) ++ [(xK_0, 0)]
+  ]
+  ++
+  [ ((mod4Mask .|. shiftMask, key), shiftToWorkspaceByPrefix $ show i)
+  | (key, i) <- (zip [xK_1..xK_9] [1..9]) ++ [(xK_0, 0)]
   ]
 
 confirmQuit :: X ()
@@ -93,6 +103,23 @@ swapScreens =
       [] -> s
       (screen:_) ->
         W.greedyView (W.tag (W.workspace screen)) s
+
+runWorkspaceAction = do
+  ws <- gets windowset
+  let tag = W.currentTag ws
+  case L.break (== '-') tag of
+    (_,[]) -> return ()
+    (_,(_:suffix)) ->  go suffix
+    where
+      go :: String -> X ()
+      go "emacs" = spawn "emacs-default" -- This is my custom wrapper around Emacs
+      go "web" = spawn "firefox --new-window"
+      go "monday" = browse "https://monday.com"
+      go "github" = browse "https://github.com"
+      go "music" = browse "https://music.youtube.com"
+      go _ = return ()
+
+browse url = spawn $ "firefox --new-window " ++ url
 
 -- Below was stolen from https://github.com/gvolpe/nix-config/blob/master/home/programs/xmonad/config.hs
 
