a7c89818160381c79d58dabfa4d511c1899976fa
Akira Komamura <akira.komamura@gmail.com>
2021-09-02 23:44:11 +0900

    emacs: Configure emacsclient

--- users/profiles/emacs/default.nix
+++ users/profiles/emacs/default.nix
@@ -6,6 +6,7 @@
       default = {
         # package = pkgs.emacs;
         useNixRun = true;
+        clientAppName = "#emacsclient";
         directory = "~/.config/emacs";
         customFile = "~/.local/share/emacs/custom.el";
         origin = "https://github.com/akirak/emacs.d.git";
